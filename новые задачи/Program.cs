710e60993141ff981080d5a358a153d721eec583
xiM1k11 <45143458+xiM1k11@users.noreply.github.com>
2023-09-14 20:09:16 +0700

    Добавьте файлы проекта.

---
+++ новые задачи/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace новые_задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
